fb43c84ee1cdb3117529186e76e781e09adb2083
Your Name <you@example.com>
2025-02-05 16:40:53 +0900

    feat: 一括代入,保守性

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -23,12 +23,15 @@ class TodoController extends Controller
 
     public function store(Request $request) 
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
 
-        $todo = new Todo(); 
-        $todo->content = $content;
+        $todo = new Todo();
+        $todo->user_id = Auth::id(); // ログインしている攻撃者のユーザID：2を代入
+        $todo->fill($inputs);
+        // $todo->content = '犯行予告など悪意のある投稿';
+        // $todo->user_id = '1'; user_idが被害者のユーザID：1に上書きされる
         $todo->save();
-
+    
         return redirect()->route('todo.index');
     } 
 
